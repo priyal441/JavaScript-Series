624b44636468d71260a6f08bd1dea4837c682d55
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-02-05 14:28:22 +0000

    container config done

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,16 @@
+// let score="33"
+// console.log(typeof score) 
+// console.log(typeof (score)) 
+// let valuenumber=Number(score)
+// console.log(typeof valuenumber) 
+
+// let isloggedin="priyal"
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin)
+//1=true 0= false
+// "" = false;
+// "priyal" true aise hoga 
+let someNUmber=33
+let StringNUmber=String(someNUmber)
+console.log(typeof StringNUmber)
+console.log(someNUmber)
