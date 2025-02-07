0b5a5326710aa1ca289f75ca7a405a5cb1f94bf3
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-02-07 19:58:53 +0000

    stringconcept

---
+++ 01_basic/string.js
@@ -0,0 +1,32 @@
+const name="Bake Bihari"
+const repocount=1600
+//  console.log(name + repocount + " value "); this is a outdated syantx we can use new version that is ` ` backtick syantax
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+//we can use this syantax 
+
+const gamename=new String('priy-al')
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+
+ const news=gamename.substring(0,4);
+//  console.log(news)
+
+
+
+// const anotherString=gamename.slice(-8,4)
+// console.log(anotherString) //yeh neg value ko ignore kr dega aur 0 se start kr dega 
+
+const newst="    priyal   ";
+console.log(newst)
+console.log(newst.trim());
+
+const url="https://priyal.com/priyal%20gupta"
+console.log(url.replace('%20','-'))
+console.log(url.includes('pyare'))
+
+
+console.log(gamename.split('-'))
+
