ad9b65ef5c8ca589a781d5ea2711076cfc1218f4
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-02-07 19:31:23 +0000

    datasummary

---
+++ 01_basic/datatypesummary.js
@@ -0,0 +1,20 @@
+//stack(primitive) . head(Non primitive)
+
+//stack example
+let myn="priyalgupta" 
+let another=myn
+another="radhe radhe"
+// console.log(myn)
+// console.log(another)
+
+
+//heap example
+let user={
+    email:"priyal441@gmail.com",
+    age:17
+}
+ let user2=user
+ user2.email="radhe@.com"
+ console.log(user.email)
+ console.log(user2.email)
+//heap k andr dono value same ho jayegi kuki iske dono copy pass nhi ki jati h vo usi ko reference deta h aur stack me uski copy pass ki jati iski vagah s dono ka opt alg aata h upr vale example m diya hua h 
