fc426078872cdbaba76709f63f18ae2340d10200
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-02-13 09:19:59 +0000

    numberandmath

---
+++ 01_basic/Numberandmath.js
@@ -0,0 +1,39 @@
+const score=400
+// console.log(score);
+
+const balance =new Number(100) //new object me define kro to vo opt me number dega
+
+// console.log(balance);
+
+// console.log( balance.toString().length);
+
+// console.log( balance.toFixed(2));//1 lege to 100.0 ayega  isme ham number dete h \
+
+const on=123.8976 //1123.8945 ka ot 1.12e+3 aayega kuki  hamne number 3 diya h 
+// console.log(on.toPrecision(4)); 
+
+const hundred=10000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//Mathss
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))//upr 
+// console.log(Math.floor(4.2)) //niche
+
+console.log(Math.random())// math.random value deti h  0 aur 1 k andr 
+console.log((Math.random()*10)+1); // math .random vala same h kuki vo 0 aur 1 k beech me
+//  value deti h isliye khi 0.1 n de  de
+//  isliye ham +1 karte h taki
+//  value badi aaye  0.1 s  kuki floor niche value select krta h ok
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)))
+
+
+
+
+
