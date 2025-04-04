0f1d1e3a95d2e98e4b16fdbd6eaca19fb0b27ef8
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-04-04 05:22:46 +0000

    true

---
+++ controlflow/truthy.js
@@ -0,0 +1,29 @@
+const useremail=[]
+// if(useremail){
+//     console.log("GOT user email")
+// }else{
+//     console.log("dont have email")
+
+// }
+
+
+
+// if(useremail.length===0){
+// console.log("array is empty")
+// }
+
+// false==0
+//op true
+// false==''
+// op true
+// o==''
+// true
+
+// const emptyobj={}
+// if(Object.keys(emptyobj).length===0){
+// console.log("object is empty")
+// }
+// falsy value
+// false,0,-0,BigInt 0n,"",null,undefined,NaN;
+// truthy Value
+// "0",'false',' ',[],{},function(){}
