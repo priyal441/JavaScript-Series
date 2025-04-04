6d77a266ff660b60c4016bbedf2c151ec792a615
Priyal Gupta <122721018+priyal441@users.noreply.github.com>
2025-04-04 05:13:29 +0000

    if

---
+++ controlflow/one.js
@@ -0,0 +1,34 @@
+// //if
+// const isuserloggedin=true
+// if(2==="2"){
+// console.log("executed");
+// }
+// console.log("hello")
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);  not defined power 
+
+
+// const balance=1000
+// // if(balance>500) console.log("test"),console.log("good") //unreadable code
+
+// if(balance<500){
+// console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 750")
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+
+const usloggedin=true
+const debitcard=true
+
+if(usloggedin&&debitcard){
+    console.log("allow to buy a course")
+}
